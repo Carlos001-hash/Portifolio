b225983b39def08a0809bafad971f4220701c6df
Carlos Eduardo <81780275+Carlos001-hash@users.noreply.github.com>
2023-12-16 00:10:33 -0300

    Update style.js

--- style.js
+++ style.js
@@ -7,18 +7,7 @@ const hoverPerfil = document.querySelector(".area-hover-pefil");
 const hoverInfo = document.querySelector(".area-hover-info");
 const projetos = document.querySelectorAll(".imgs-conteudo")
 
-projeto1.addEventListener("mouseover", ()=>{ projeto1.style = "width: 285px; height: 285px; box-shadow: 6px 6px;"})
-projeto1.addEventListener("mouseout", ()=>{ projeto1.style = "width: 280px; height: 280px; box-shadow: 0px;"})
 
-projeto2.addEventListener("mouseover", ()=>{ projeto2.style = "width: 285px; height: 285px; box-shadow: 6px 6px;"})
-projeto2.addEventListener("mouseout", ()=>{ projeto2.style = "width: 280px; height: 280px; box-shadow: 0px;"})
-
-projeto3.addEventListener("mouseover", ()=>{ projeto3.style = "width: 285px; height: 285px; box-shadow: 6px 6px;"})
-projeto3.addEventListener("mouseout", ()=>{ projeto3.style = "width: 280px; height: 280px; box-shadow: 0px;"})
-
-projeto4.addEventListener("mouseover", ()=>{ projeto4.style = "width: 285px; height: 285px; box-shadow: 6px 6px;"})
-projeto4.addEventListener("mouseout", ()=>{ projeto4.style = "width: 280px; height: 280px; box-shadow: 0px;"})
-
-hoverPerfil.addEventListener("mouseover", ()=>{ hoverInfo.style = "display: flex;"})
+hoverPerfil.addEventListener("mouseover", ()=>{ hoverInfo.style = "display: flex;"});
  
-hoverPerfil.addEventListener("mouseout", ()=>{ hoverInfo.style = "display: none;"}) 
+hoverPerfil.addEventListener("mouseout", ()=>{ hoverInfo.style = "display: none;"});
